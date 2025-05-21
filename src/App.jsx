9ab1c353539843674f0dc8828adcfd60d42cc596
kovbinka <roman10kovbinka@gmail.com>
2025-05-21 13:00:54 +0200

    separated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,23 @@ import Display from './components/Display'
 import ButtonPanel from './components/ButtonPanel'
 import Logo from './components/Logo'
 import Header from './components/Header'
+import { handleButtonClick } from './utils/utils'
 import './App.css'
 
 
 function App() {
   const [input, setInput] = useState('');
 
-    const handleButtonClick = (value) => {
-        if (value === '=') {
-            try {
-                setInput(eval(input).toString());
-            } catch (error) {
-                setInput('Error');
-            }
-        } else if (value === 'C') {
-          setInput('');
-        } else {
-            setInput(input + value);
-        }
-    };
-
   return (
     <>
       <Header title='React Calculator'/>
       <Logo />
-    <div className="calculator">
-      <Display value={input} />
-      <ButtonPanel onButtonClick={handleButtonClick} />
-    </div>
+      <div className="calculator">
+        <Display value={input} />
+        <ButtonPanel onButtonClick={(value) => handleButtonClick(value, input, setInput)} />
+      </div>
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/utils/utils.js
@@ -0,0 +1,13 @@
+export const handleButtonClick = (value, input, setInput) => {
+    if (value === '=') {
+      try {
+        setInput(eval(input).toString());
+      } catch (error) {
+        setInput('Error');
+      }
+    } else if (value === 'C') {
+      setInput('');
+    } else {
+      setInput(input + value);
+    }
+  };
